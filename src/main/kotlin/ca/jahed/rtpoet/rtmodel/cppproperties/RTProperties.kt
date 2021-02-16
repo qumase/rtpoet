cc7c680e4caa288a8d43de4b48755c1524342b8f
Karim Jahed <jahed@cs.queensu.ca>
2021-02-15 19:48:16 -0500

    Fixed stackoverflow bug when using EqualityHelper

--- src/main/kotlin/ca/jahed/rtpoet/rtmodel/cppproperties/RTProperties.kt
+++ src/main/kotlin/ca/jahed/rtpoet/rtmodel/cppproperties/RTProperties.kt
@@ -2,4 +2,14 @@ package ca.jahed.rtpoet.rtmodel.cppproperties
 
 import ca.jahed.rtpoet.rtmodel.RTElement
 
-abstract class RTProperties : RTElement()
+abstract class RTProperties : RTElement() {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return javaClass.hashCode()
+    }
+}
