38395dd6343739cd576b40b97a2e6b69cee318f1
Karim Jahed <jahed@cs.queensu.ca>
2021-05-11 09:43:59 -0400

    Added depth visitor & qualified name helper

---
+++ src/main/kotlin/ca/jahed/rtpoet/rtmodel/visitors/RTDepthVisitor.kt
@@ -0,0 +1,112 @@
+package ca.jahed.rtpoet.rtmodel.visitors
+
+import ca.jahed.rtpoet.rtmodel.*
+import ca.jahed.rtpoet.rtmodel.sm.RTCompositeState
+import ca.jahed.rtpoet.rtmodel.sm.RTState
+import ca.jahed.rtpoet.rtmodel.sm.RTStateMachine
+import ca.jahed.rtpoet.rtmodel.sm.RTTransition
+
+open class RTDepthVisitor(private val model: RTModel) : RTVisitor() {
+    override fun visitModel(model: RTModel): Any {
+        visitPackage(model)
+        if (model.top != null) visit(model.top!!)
+        return super.visitModel(model)
+    }
+
+    override fun visitPackage(pkg: RTPackage): Any {
+        model.capsules.forEach { visit(it) }
+        model.classes.forEach { visit(it) }
+        model.artifacts.forEach { visit(it) }
+        model.enumerations.forEach { visit(it) }
+        model.protocols.forEach { visit(it) }
+        model.packages.forEach { visit(it) }
+        return super.visitPackage(pkg)
+    }
+
+    override fun visitCapsule(capsule: RTCapsule): Any {
+        capsule.attributes.forEach { visit(it) }
+        capsule.operations.forEach { visit(it) }
+
+        capsule.parts.forEach { visit(it) }
+        capsule.ports.forEach { visit(it) }
+        capsule.connectors.forEach { visit(it) }
+        if (capsule.stateMachine != null) visit(capsule.stateMachine!!)
+        if (capsule.properties != null) visit(capsule.properties!!)
+        return super.visitCapsule(capsule)
+    }
+
+    override fun visitClass(klass: RTClass): Any {
+        klass.attributes.forEach { visit(it) }
+        klass.operations.forEach { visit(it) }
+        if (klass.properties != null) visit(klass.properties!!)
+        return super.visitClass(klass)
+    }
+
+    override fun visitProtocol(protocol: RTProtocol): Any {
+        protocol.inputSignals.forEach { visit(it) }
+        protocol.outputSignals.forEach { visit(it) }
+        protocol.inOutSignals.forEach { visit(it) }
+        return super.visitProtocol(protocol)
+    }
+
+    override fun visitSignal(signal: RTSignal): Any {
+        signal.parameters.forEach { visit(it) }
+        return super.visitSignal(signal)
+    }
+
+    override fun visitOperation(operation: RTOperation): Any {
+        operation.parameters.forEach { visit(it) }
+        if (operation.ret != null) visit(operation.ret!!)
+        if (operation.action != null) visit(operation.action!!)
+        if (operation.properties != null) visit(operation.properties!!)
+        return super.visitOperation(operation)
+    }
+
+    override fun visitStateMachine(statemachine: RTStateMachine): Any {
+        statemachine.states().forEach { visit(it) }
+        statemachine.transitions().forEach { visit(it) }
+        return super.visitStateMachine(statemachine)
+    }
+
+    override fun visitCompositeState(state: RTCompositeState): Any {
+        if (state.entryAction != null) visit(state.entryAction!!)
+        if (state.exitAction != null) visit(state.exitAction!!)
+        state.states().forEach { visit(it) }
+        state.transitions().forEach { visit(it) }
+        return super.visitCompositeState(state)
+    }
+
+
+    override fun visitState(state: RTState): Any {
+        if (state.entryAction != null) visit(state.entryAction!!)
+        if (state.exitAction != null) visit(state.exitAction!!)
+        return super.visitState(state)
+    }
+
+    override fun visitTransition(transition: RTTransition): Any {
+        if (transition.guard != null) visit(transition.guard!!)
+        if (transition.action != null) visit(transition.action!!)
+        transition.triggers.forEach { visit(it) }
+        return super.visitTransition(transition)
+    }
+
+    override fun visitEnumeration(enumeration: RTEnumeration): Any {
+        if (enumeration.properties != null) visit(enumeration.properties!!)
+        return super.visitEnumeration(enumeration)
+    }
+
+    override fun visitArtifact(artifact: RTArtifact): Any {
+        if (artifact.properties != null) visit(artifact.properties!!)
+        return super.visitArtifact(artifact)
+    }
+
+    override fun visitAttribute(attribute: RTAttribute): Any {
+        if (attribute.properties != null) visit(attribute.properties!!)
+        return super.visitAttribute(attribute)
+    }
+
+    override fun visitParameter(param: RTParameter): Any {
+        if (param.properties != null) visit(param.properties!!)
+        return super.visitParameter(param)
+    }
+}
---
+++ src/main/kotlin/ca/jahed/rtpoet/utils/RTQualifiedNameHelper.kt
@@ -0,0 +1,29 @@
+package ca.jahed.rtpoet.utils
+
+import ca.jahed.rtpoet.rtmodel.RTElement
+import ca.jahed.rtpoet.rtmodel.RTModel
+import ca.jahed.rtpoet.rtmodel.visitors.RTDepthVisitor
+
+class RTQualifiedNameHelper(model: RTModel) : RTDepthVisitor(model) {
+    private val qualifiedNames = mutableMapOf<RTElement, String>()
+    private val currentPrefix = mutableListOf<String>()
+
+    init {
+        visit(model)
+    }
+
+    fun getQualifiedName(element: RTElement): String {
+        return qualifiedNames[element]!!
+    }
+
+    fun getQualifiedNames(): MutableMap<RTElement, String> {
+        return qualifiedNames
+    }
+
+    override fun visit(element: RTElement) {
+        currentPrefix.add(element.name)
+        qualifiedNames[element] = currentPrefix.joinToString(".")
+        super.visit(element)
+        currentPrefix.removeLast()
+    }
+}
